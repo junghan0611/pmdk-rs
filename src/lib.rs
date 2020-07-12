ce23062b993bf45d28cae9715b4117d600e569e6
Cyril Plisko <cyril.plisko@replix.io>
2020-07-12 12:51:48 +0000

    Bump version to 0.9.2

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@
 // Use is subject to license terms.
 //
 
-#![doc(html_root_url = "https://docs.rs/pmdk/0.8.0")]
+#![doc(html_root_url = "https://docs.rs/pmdk/0.9.2")]
 #![warn(clippy::use_self)]
 #![warn(deprecated_in_future)]
 #![warn(future_incompatible)]
